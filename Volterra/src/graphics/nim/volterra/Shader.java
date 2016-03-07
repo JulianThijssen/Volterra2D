ee2d8e73c87bc1116164ef77c73687b5c3497519
JulianThijssen <Nimthora@gmail.com>
2016-03-07 01:50:09 +0100

    Added location hashing in shader

--- Volterra/src/graphics/nim/volterra/Shader.java
+++ Volterra/src/graphics/nim/volterra/Shader.java
@@ -1,13 +1,24 @@
 package graphics.nim.volterra;
 
+import java.util.HashMap;
+
+import static org.lwjgl.opengl.GL20.glGetUniformLocation;
+
 public class Shader {
 	public int handle;
+	
+	private HashMap<String, Integer> locationMap = new HashMap<String, Integer>();
 	
 	public Shader(int handle) {
 		this.handle = handle;
 	}
 	
-	public void location() {
-		
+	public int location(String name) {
+		Integer location = locationMap.get(name);
+		if (location == null) {
+			location = glGetUniformLocation(handle, name);
+			locationMap.put(name, location);
+		}
+		return location;
 	}
 }

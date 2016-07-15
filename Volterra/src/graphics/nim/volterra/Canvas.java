3bbc6d9ce52d345a1662e075f45e77cf090feb7c
JulianThijssen <Nimthora@gmail.com>
2016-07-16 01:22:26 +0200

    Added canvas documentation

--- Volterra/src/graphics/nim/volterra/Canvas.java
+++ Volterra/src/graphics/nim/volterra/Canvas.java
@@ -25,20 +25,63 @@ public class Canvas {
 	private static final float Z_FAR = 1000;
 	private static final float Z_RANGE = Z_FAR - Z_NEAR;
 	
+	/** The internal quad for drawing all on-screen sprites.*/
+	private int quad;
+	
+	/** The currently bound shader for drawing.*/
 	private Shader shader;
 	
+	/** The current projection matrix, this matrix can be changed with {@link #setBounds(float, float, float, float) setBounds}.*/
 	private Matrix4f projMatrix = new Matrix4f();
+	/** The current view matrix, this matrix can be changed by a {@link graphics.nim.volterra.Camera Camera} object.*/
 	private Matrix4f viewMatrix = new Matrix4f();
+	/** The current model matrix, this matrix is changed in order to transform sprites.*/
 	private Matrix4f modelMatrix = new Matrix4f();
 	
+	/** Contains the bounds of the screen. */
 	private Bounds bounds = new Bounds(-1, 1, -1, 1);
 	
-	private Font font;
+	/** The current color with which all shapes are drawn.*/
 	private Vector3f color = new Vector3f(1, 1, 1);
+	
+	/** The currently bound font with which text is drawn.*/
+	private Font font;
+	/** The current color with which all text is drawn.*/
 	private Vector3f fontColor = new Vector3f(1, 1, 1);
-	private int quad = ShapeLoader.getQuad();
 
+	/**
+	 * Class constructor.
+	 */
 	public Canvas() {
+		init();
+	}
+	
+	/**
+	 * Sets the currently bound shader for drawing.
+	 * 
+	 * @param name The name under which the shader is stored in {@link graphics.nim.volterra.Resources Resources}.
+	 */
+	public void setShader(String name) {
+		shader = Resources.getShader(name);
+		shader.bind();
+		shader.uniformMatrix4f("projMatrix", projMatrix);
+		shader.uniformMatrix4f("viewMatrix", viewMatrix);
+	}
+	
+	/**
+	 * Sets the font with which all further text is drawn.
+	 * 
+	 * @param font The font to use for text drawing.
+	 */
+	public void setFont(Font font) {
+		this.font = font;
+	}
+	
+	/**
+	 * Sets the initial state of the canvas.
+	 */
+	private void init() {
+		quad = ShapeLoader.getQuad();
 		font = FontLoader.loadFont("Arial", 50);
 		
 		glEnable(GL_BLEND);
@@ -48,33 +91,42 @@ public class Canvas {
 		setShader("Unlit");
 	}
 	
-	public void setShader(String name) {
-		shader = Resources.getShader(name);
-		shader.bind();
-		shader.uniformMatrix4f("projMatrix", projMatrix);
-		shader.uniformMatrix4f("viewMatrix", viewMatrix);
-	}
-	
-	public void setFont(Font font) {
-		this.font = font;
-	}
-	
-	public void init() {
-		
-	}
-	
+	/**
+	 * Sets the color the canvas is cleared to upon calling {@link #clear() clear}.
+	 * 
+	 * @param r The red component of the color in the range [0-1].
+	 * @param g The green component of the color in the range [0-1].
+	 * @param b The blue component of the color in the range [0-1].
+	 */
 	public void setClearColor(float r, float g, float b) {
 		glClearColor(r, g, b, 1);
 	}
 	
+	/**
+	 * Sets the color with which further shapes are drawn.
+	 * 
+	 * @param r The red component of the color in the range [0-1].
+	 * @param g The green component of the color in the range [0-1].
+	 * @param b The blue component of the color in the range [0-1].
+	 */
 	public void setColor(float r, float g, float b) {
 		color.set(r, g, b);
 	}
 	
+	/**
+	 * Sets the color with which further text is drawn.
+	 * 
+	 * @param r The red component of the color in the range [0-1].
+	 * @param g The green component of the color in the range [0-1].
+	 * @param b The blue component of the color in the range [0-1].
+	 */
 	public void setFontColor(float r, float g, float b) {
 		fontColor.set(r, g, b);
 	}
 	
+	/**
+	 * Takes a screenshot of the current framebuffer.
+	 */
 	public void takeScreenshot() {
 		glReadBuffer(GL_FRONT);
 
@@ -105,6 +157,14 @@ public class Canvas {
 		}
 	}
 	
+	/**
+	 * Sets the bounds of the screen.
+	 * 
+	 * @param left The left boundary of the screen.
+	 * @param right The right boundary of the screen.
+	 * @param bottom The bottom boundary of the screen.
+	 * @param top The top boundary of the screen.
+	 */
 	public void setBounds(float left, float right, float bottom, float top) {
 		projMatrix.setIdentity();
 
@@ -120,6 +180,12 @@ public class Canvas {
 		glViewport(0, 0, Window.width, Window.height);
 	}
 	
+	/**
+	 * Sets the camera with which the scene is rendered.
+	 * Internally this method sets the viewMatrix member variable.
+	 * 
+	 * @param camera The camera with which to render the scene.
+	 */
 	public void setCamera(Camera camera) {
 		Vector2f position = camera.getPosition();
 		float rotation = camera.getRotation();
@@ -131,10 +197,24 @@ public class Canvas {
 		viewMatrix.scale(new Vector3f(zoom.x, zoom.y, 1));
 	}
 	
-	public void bindTexture(Texture texture) {
+	/**
+	 * Binds a texture to an OpenGL texture slot.
+	 * Internally this method makes the texture slot active
+	 * before binding the given texture. The slot should
+	 * be a constant integer instead of an OpenGL enum
+	 * (0 instead of GL_TEXTURE0).
+	 * 
+	 * @param slot The texture slot to bind the texture to.
+	 * @param texture The texture to bind.
+	 */
+	public void bindTexture(int slot, Texture texture) {
+		glActiveTexture(GL_TEXTURE0 + slot);
 		glBindTexture(GL_TEXTURE_2D, texture.getHandle());
 	}
 	
+	/**
+	 * Clears the color and depth buffer
+	 */
 	public void clear() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shader.bind();
@@ -143,12 +223,35 @@ public class Canvas {
 		glActiveTexture(GL_TEXTURE0);
 	}
 	
+	/**
+	 * Draws a rectangle at position x,y with size w x h.
+	 * The color of the rectangle can be set with {@link #setColor(float, float, float) setColor}.
+	 * 
+	 * @param x The x-component of the position of the rectangle.
+	 * @param y The y-component of the position of the rectangle.
+	 * @param w The width of the rectangle.
+	 * @param h The height of the rectangle.
+	 */
 	public void drawRect(float x, float y, float w, float h) {
+		drawRect(x, y, 0, w, h);
+	}
+	
+	/**
+	 * Draws a rectangle at position x,y,d with size w x h.
+	 * The color of the rectangle can be set with {@link #setColor(float, float, float) setColor}.
+	 * 
+	 * @param x The x-component of the position of the rectangle.
+	 * @param y The y-component of the position of the rectangle.
+	 * @param d The depth of the position of the rectangle.
+	 * @param w The width of the rectangle.
+	 * @param h The height of the rectangle.
+	 */
+	public void drawRect(float x, float y, float d, float w, float h) {
 		shader.uniform1i("hasTexture", 0);
 		shader.uniform3f("color", color.x, color.y, color.z);
 		
 		modelMatrix.setIdentity();
-		modelMatrix.translate(new Vector3f(x, y, 0));
+		modelMatrix.translate(new Vector3f(x, y, d));
 		modelMatrix.scale(new Vector3f(w, h, 1));
 		shader.uniformMatrix4f("modelMatrix", modelMatrix);
 		
@@ -156,6 +259,14 @@ public class Canvas {
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 	}
 	
+	/**
+	 * Draws a texture at position x,y with a given depth value.
+	 * 
+	 * @param x The x-component of the position of the texture.
+	 * @param y The y-component of the position of the texture.
+	 * @param depth The depth of the position of the texture.
+	 * @param image The texture to be rendered.
+	 */
 	public void drawImage(float x, float y, float depth, Texture image) {
 		glBindTexture(GL_TEXTURE_2D, image.getHandle());
 		shader.uniform1i("hasTexture", 1);
@@ -171,6 +282,14 @@ public class Canvas {
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 	}
 
+	/**
+	 * Draws a sprite at position x,y with a given depth value.
+	 * 
+	 * @param x The x-component of the position of the sprite.
+	 * @param y The y-component of the position of the sprite.
+	 * @param depth The depth of the position of the sprite.
+	 * @param sprite The sprite to be rendered.
+	 */
 	public void drawImage(float x, float y, float depth, Sprite sprite) {
 		glBindTexture(GL_TEXTURE_2D, sprite.getTextureHandle());
 		shader.uniform1i("hasTexture", 1);
@@ -196,6 +315,15 @@ public class Canvas {
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 	}
 	
+	/**
+	 * Draws the specified string at position x,y and the specified depth
+	 * 
+	 * @param text The text to be rendered.
+	 * @param x The x-component of the position of the text.
+	 * @param y The y-component of the position of the text.
+	 * @param depth The depth of the position of the text.
+	 * @param scale The scale with which to resize the text.
+	 */
 	public void drawString(String text, int x, int y, float depth, float scale) {
 		float stride = 0;
 

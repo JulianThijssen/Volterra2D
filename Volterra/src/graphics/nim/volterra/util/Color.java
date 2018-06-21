3c16de41c75760bb8ef0f2856c14b32d0e627162
Julian Thijssen <Nimthora@gmail.com>
2018-06-21 14:18:36 +0200

    Remake Color class to be more sensible

--- Volterra/src/graphics/nim/volterra/util/Color.java
+++ Volterra/src/graphics/nim/volterra/util/Color.java
@@ -1,23 +1,36 @@
 package graphics.nim.volterra.util;
 
-import java.nio.FloatBuffer;
-
 public class Color {
-	public float r = 1;
-	public float g = 1;
-	public float b = 1;
+	public static Color WHITE      = new Color(1, 1, 1, 1);
+	public static Color BLACK      = new Color(0, 0, 0, 1);
+	public static Color RED        = new Color(1, 0, 0, 1);
+	public static Color GREEN      = new Color(0, 1, 0, 1);
+	public static Color BLUE       = new Color(0, 0, 1, 1);
+	public static Color GREY       = new Color(0.5f, 0.5f, 0.5f, 1);
+	public static Color YELLOW     = new Color(1.0f, 1.0f, 0.0f, 1);
+	public static Color MAGENTA    = new Color(1.0f, 0.0f, 1.0f, 1);
+	public static Color CYAN       = new Color(0.0f, 1.0f, 1.0f, 1);
+	public static Color PINK       = new Color(1.0f, 0.5f, 0.5f, 1);
+	public static Color LIME       = new Color(0.5f, 1.0f, 0.5f, 1);
+	
+	public float r = 0;
+	public float g = 0;
+	public float b = 0;
 	public float a = 1;
 	
-	public Color() {
-		
+	public Color(float r, float g, float b) {
+		set(r, g, b);
 	}
 	
 	public Color(float r, float g, float b, float a) {
 		set(r, g, b, a);
 	}
 	
-	public Color(float r, float g, float b) {
-		set(r, g, b);
+	public void set(float r, float g, float b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		clamp();
 	}
 	
 	public void set(float r, float g, float b, float a) {
@@ -25,84 +38,41 @@ public class Color {
 		this.g = g;
 		this.b = b;
 		this.a = a;
+		clamp();
 	}
 	
-	public void set(float r, float g, float b) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
-
-	public void set(Color v) {
-		this.r = v.r;
-		this.g = v.g;
-		this.b = v.b;
-		this.a = v.a;
-	}
-	
-	public Color add(Color v) {
-		this.r += v.r;
-		this.g += v.g;
-		this.b += v.b;
+	public Color add(Color c) {
+		this.r += c.r;
+		this.g += c.g;
+		this.b += c.b;
+		clamp();
 		return this;
 	}
 	
-	public Color sub(Color v) {
-		this.r -= v.r;
-		this.g -= v.g;
-		this.b -= v.b;
+	public Color sub(Color c) {
+		this.r -= c.r;
+		this.g -= c.g;
+		this.b -= c.b;
+		clamp();
 		return this;
 	}
 	
-	public Color scale(float scale) {
-		this.r *= scale;
-		this.g *= scale;
-		this.b *= scale;
-		return this;
+	private float clamp(float f) {
+		return f > 1 ? 1 : f < 0 ? 0 : f;
 	}
 	
-	public float length() {
-		return (float) Math.sqrt(r * r + g * g + b * b);
+	private void clamp() {
+		r = clamp(r);
+		g = clamp(g);
+		b = clamp(b);
+		a = clamp(a);
 	}
 	
-	public Color normalise() {
-		float length = length();
-		this.r /= length;
-		this.g /= length;
-		this.b /= length;
-		return this;
+	public static Color add(Color c1, Color c2) {
+		return new Color(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b);
 	}
 	
-	public void load(FloatBuffer buf) {
-		r = buf.get();
-		g = buf.get();
-		b = buf.get();
-	}
-	
-	public void store(FloatBuffer buf) {
-		buf.put(r);
-		buf.put(g);
-		buf.put(b);
-	}
-
-	@Override
-	public String toString() {
-		return "("+r+", "+g+", "+b+")";
-	}
-	
-	public static Color add(Color v1, Color v2) {
-		return new Color(v1.r + v2.r, v1.g + v2.g, v1.b + v2.b);
-	}
-	
-	public static Color sub(Color v1, Color v2) {
-		return new Color(v1.r - v2.r, v1.g - v2.g, v1.b - v2.b);
-	}
-	
-	public static Color scale(Color v, float scale) {
-		return new Color(v.r * scale, v.g * scale, v.b * scale);
-	}
-	
-	public static float dot(Color v1, Color v2) {
-		return v1.r * v2.r + v1.g * v2.g + v1.b * v2.b;
+	public static Color sub(Color c1, Color c2) {
+		return new Color(c1.r - c2.r, c1.g - c2.g, c1.b - c2.b);
 	}
 }
